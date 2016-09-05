fad379c37b2f2b1384cd718519a19507337c8f5b
mike-hb <hartje@etech.hs-bremen.de>
2016-09-05 23:05:14 +0200

    restructured convert routines, integer fields
    combined conversion uploding only without -fo outfile
    fields for integer have an additional "i" - e.g. 4i

--- wspr-to-influxdb.py
+++ wspr-to-influxdb.py
@@ -20,7 +20,7 @@ The upload of the output file (-fo output.daten) could be done with
 
 Function:
 reading from -fi Infile preparing the ALL_WSPR.TXT-format for upload
-uploading with json-client to influxDB-Server -host 
+uploading with json-client to influxDB-Server -host if not given -fo outfile
 
 Installation:
 Prepare the python installation depending on what python version you are
@@ -61,83 +61,7 @@ def haversine(lat1, lon1, lat2, lon2):
     return int(km), int(Bearing)
 
 
-def wspr_to_file(in_str,wspr_reporter,wspr_loc_reporter, fout):
-
-    # implemented for legacy compatibility
-    # in_str = '160310 1942   1 -24 -4.0  7.0395714  PA2W JO22 37            1  6739  -48'
-
-    in_str = in_str.replace('     ', ' ')
-    in_str = in_str.replace('    ', ' ')
-    in_str = in_str.replace('   ', ' ')
-    in_str = in_str.replace('  ', ' ')
-
-    wspr_date, wspr_time, wspr_other, wspr_snr, wspr_dt, wspr_freq, wspr_call, \
-         wspr_loc, wspr_pwr, wspr_drift, wspr_rest = in_str.split(' ', 10)
-
-    band_vec = (2200,630,160,80,60,60,40,30,20,17,15,12,10,6)
-    freq_vec = (0.1,0.4,1.8,3.5,5.2,5.3,7.0,10.1,14.0,18.1,21.0,24.9,28.1,50.2)
-    wspr_band = band_vec[freq_vec.index(round(float(wspr_freq) - 0.05,1))]
-
-#    band_vec = ('LF', 'MW', '160m', '80m', '60m', '40m', '30m', '20m', '17m', '15m', '12m', '10m', '6m')
-#    freq_vec = (0.1, 0.4, 1.8, 3.5, 5.2, 7.0, 10.1, 14.0, 18.1, 21.0, 24.9, 28.1, 50.2)
-#    wspr_band = band_vec[freq_vec.index(round(float(wspr_freq) - 0.05, 1))]
-
-    wspr_tuple_time = strptime(wspr_date + wspr_time, "%y%m%d%H%M")
-    wspr_time = strftime("%Y-%m-%dT%H:%M:%SZ", wspr_tuple_time)
-
-    loclon_reporter = mlocs.toLoc(wspr_loc_reporter)
-    wspr_geohash_reporter = Geohash.encode(loclon_reporter[0], loclon_reporter[1], precision=7)
-
-    calckm_az = False
-    if wspr_call.find('/') < 0 or wspr_call.find('<') == 0:
-        calckm_az = True
-
-    else:
-        wspr_dist = -1
-        wspr_az = -1
-        wspr_drift = wspr_pwr
-        wspr_pwr = wspr_loc
-        wspr_loc = 'AA00'
-        wspr_geohash = '0000'
-
-    if wspr_call.find('<') == 0:
-        wspr_call = wspr_call[1:-1]
-        if wspr_call.find('..') >= 0:
-            wspr_call = "00" + wspr_loc
-
-    wspr_call = re.sub('[<>]', '', wspr_call)
-
-    if wspr_call.find('..') >= 0:
-        wspr_call = "00" + wspr_loc
-
-    if calckm_az:
-        loclon = mlocs.toLoc(wspr_loc)
-        wspr_dist, wspr_az = haversine(loclon_reporter[0], loclon_reporter[1], loclon[0], loclon[1])
-        wspr_geohash = Geohash.encode(loclon[0], loclon[1], precision=7)
-
-    dat_str_local = 'wspr_redpitaya' + \
-                    ',reporter=' + str(wspr_reporter) + \
-                    ',call=' + wspr_call + \
-                    ',band=' + str(wspr_band).rjust(4, '.') + \
-                    ',loc=' + wspr_loc + \
-                    ',loc_reporter=' + wspr_loc_reporter + \
-                    ',geohash=' + str(wspr_geohash) + \
-                    ',geohash_reporter=' + str(wspr_geohash_reporter) + \
-                    ' snr=' + str(wspr_snr) + \
-                    ',freq=' + str("%.6f" % float(wspr_freq)) + \
-                    ',drift=' + str(int(wspr_drift)) + \
-                    ',dt=' + str("%.1f" % float(wspr_dt)) + \
-                    ',dist=' + str(wspr_dist) + \
-                    ',az=' + str(wspr_az) + \
-                    ',bandi=' + str(wspr_band) + \
-                    ',pwr=' + str(wspr_pwr) + \
-                    ' ' + str(wspr_time)
-
-
-    fout.write(dat_str_local + '\n')
-
-
-def wspr_to_json(in_str,wspr_reporter,wspr_loc_reporter):
+def wspr_to_upload(in_str,wspr_reporter,wspr_loc_reporter):
 
     # in_str = '160310 1942   1 -24 -4.0  7.0395714  PA2W JO22 37            1  6739  -48'
 
@@ -205,20 +129,40 @@ def wspr_to_json(in_str,wspr_reporter,wspr_loc_reporter):
                 "geohash_reporter": wspr_geohash_reporter
             },
             "time": wspr_time,
+            # fields should be real(integer values -- not strings
             "fields": {
-                "snr": int(str(wspr_snr)),
-                # limit freq to 6 digits, but keep it a float, no string!
-                "freq": float("%.6f" % float(wspr_freq)),  
-                "drift": int(str(wspr_drift)),
-                "dt": str("%.1f" % float(wspr_dt)),
-                "dist": wspr_dist,
-                "az": wspr_az,
-                "bandi": wspr_band,
-                "pwr": int(str(wspr_pwr))
+                "snr": ("%ii" % wspr_snr),
+                # limit freq to 6 digits, but keep it a float, not a string!
+                "freq": ("%.6f" % float(wspr_freq)),  
+                "drift": ("%ii" % wspr_drift),
+                "dt": ("%.1f" % float(wspr_dt)),
+                "dist": "%ii" % wspr_dist,
+                "az": "%ii" % wspr_az,
+                "bandi": "%ii" % wspr_band,
+                "pwr": "%ii" % wspr_pwr # dBm
             }
         }
     ]
-    return json_body
+    
+    dat_str_local = 'wspr_redpitaya' + \
+                    ',reporter=' + str(wspr_reporter) + \
+                    ',call=' + wspr_call + \
+                    ',band=' + str(wspr_band).rjust(4, '.') + \
+                    ',loc=' + wspr_loc + \
+                    ',loc_reporter=' + wspr_loc_reporter + \
+                    ',geohash=' + str(wspr_geohash) + \
+                    ',geohash_reporter=' + str(wspr_geohash_reporter) + \
+                    ' snr=' + str(wspr_snr) + 'i' +\
+                    ',freq=' + str("%.6f" % float(wspr_freq)) + \
+                    ',drift=' + str(int(wspr_drift)) + 'i' + \
+                    ',dt=' + str("%.1f" % float(str(wspr_dt))) + \
+                    ',dist=' + str(wspr_dist) + 'i' + \
+                    ',az=' + str(wspr_az) + 'i' + \
+                    ',bandi=' + str(wspr_band) + 'i' + \
+                    ',pwr=' + str(wspr_pwr) + 'i' + \
+                    ' ' + str(wspr_time)
+                    
+    return json_body, dat_str_local
 
 if __name__ == '__main__':  # noqa
     import re
@@ -230,8 +174,8 @@ if __name__ == '__main__':  # noqa
     import Geohash
 
     parser = argparse.ArgumentParser(
-        description='Load ALL_WSPR.TXT like file to Influxdb',
-        epilog="""... epilog ... no more info atm ...""")
+        description='Load ALL_WSPR.TXT like file to Influxdb (or prepare into -fo file)',
+        epilog="""... epilog ... with param -fo there is no included upload ...""")
 
     parser.add_argument(
         '-r', '--reporter',
@@ -300,29 +244,32 @@ if __name__ == '__main__':  # noqa
         try:
             if args.fo:
                 fout = open(args.fo,'a')
-                print "Additional output to {}.\n".format(args.fo)
-            # open connection to Influxdb
-            client = InfluxDBClient(args.host, args.port, args.user, 
+                print "output to {} ...".format(args.fo)
+            # open connection to Influxdb, fixed DB "wspr"
+            else:
+                client = InfluxDBClient(args.host, args.port, args.user, 
                                     args.password, 'wspr')
             # iterate over lines
             wspr_no = sum(1 for line in f)
-            f.seek(0, 0)
+            f.seek(0)
             i = 1
             for in_str in f:
 #                print "{}/{}".format(i,wspr_no)
                 #print(in_str)
-                json_body = wspr_to_json(in_str,args.reporter,args.reporter_locator)
+                json_body,dat_str = wspr_to_upload(in_str,args.reporter,args.reporter_locator)
                 #print(json_body)
 
                 if args.fo:
-                    wspr_to_file(in_str, args.reporter, args.reporter_locator, fout)
+                    fout.write(dat_str + '\n')
                 else:
                     # submit spot to Influxdb
                     ret = client.write_points(json_body)
                 i=i+1
-            print( str(wspr_no) + " spot Uploads to influxdB done.")
+            if args.fo:
+                print( str(wspr_no) + " spot for influxdB to file.")
+            else:    
+                print( str(wspr_no) + " spot Uploads to influxdB done.")
         finally:
             f.close()
             if args.fo:
                 fout.close()
-
